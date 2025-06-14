a454773c2f2c470250c8a3d21bd11f4a357aac41
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-06-14 23:27:38 +0200

    added nuxtlink to button

--- components/elements/Button.vue
+++ components/elements/Button.vue
@@ -1,21 +1,22 @@
 <template>
-  <button
-    @click="navigateTo({ path: link, hash: hash })"
-    class="min-w-40 rounded px-6 py-3 text-center text-sm text-black no-underline duration-200"
-    :class="[
-      styling === 'dark' ? 'bg-neutral-800 text-white' : 'bg-white/95',
-      outline
-        ? styling === 'dark'
-          ? 'border-2 border-white'
-          : 'border-2 border-neutral-900'
-        : '',
-    ]"
-    :type="type"
-    :data-wait="dataWait"
-    text=""
-  >
-    {{ text }}
-  </button>
+  <NuxtLink :to="{ path: link, hash: hash }">
+    <button
+      class="min-w-40 cursor-pointer rounded px-6 py-3 text-center text-sm text-black no-underline duration-200"
+      :class="[
+        styling === 'dark' ? 'bg-neutral-800 text-white' : 'bg-white/95',
+        outline
+          ? styling === 'dark'
+            ? 'border-2 border-white'
+            : 'border-2 border-neutral-900'
+          : '',
+      ]"
+      :type="type"
+      :data-wait="dataWait"
+      text=""
+    >
+      {{ text }}
+    </button>
+  </NuxtLink>
 </template>
 
 <script>
