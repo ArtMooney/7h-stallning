2ec1d0a341967a7b0fd443f329de0e7bbbf8e04b
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-06-15 22:05:11 +0200

    added updates to pointblob

--- components/PointBlob.vue
+++ components/PointBlob.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="h-fit w-fit">
-    <div class="flex flex-col gap-2 p-8">
-      <h4 v-if="title" class="font-bold">{{ title }}</h4>
+    <div class="gap-2 p-8">
+      <h4 v-if="title" class="font-bold" :class="getPointWidth">{{ title }}</h4>
 
       <ul v-if="pointLines" class="flex flex-col space-y-4 leading-normal">
-        <li v-for="line in pointLines">{{ line }}</li>
+        <li v-for="line in pointLines">
+          <span v-if="getPointSymbol" class="absolute" :class="getPointWidth">{{
+            getPointSymbol
+          }}</span>
+          {{ line }}
+        </li>
       </ul>
     </div>
   </div>
@@ -22,6 +27,30 @@ export default {
     pointLines: {
       type: Array,
       required: false,
+    },
+    pointType: {
+      type: String,
+      required: false,
+    },
+    pointWidth: {
+      type: String, // small, medium, large
+      required: false,
+    },
+  },
+
+  computed: {
+    getPointSymbol() {
+      if (this.pointType === "dot") return "•";
+      if (this.pointType === "checkbox") return "✅";
+      if (this.pointType === "arrow") return "→";
+      return "";
+    },
+
+    getPointWidth() {
+      if (this.pointWidth === "small") return "-ml-3";
+      if (this.pointWidth === "medium") return "-ml-6";
+      if (this.pointWidth === "large") return "-ml-9";
+      return "";
     },
   },
 };
