46daaba28c933cd78298a5742eff0e4e0bf04ebe
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-06-17 21:54:46 +0200

    removed specific links

--- components/SocialMediaIcons.vue
+++ components/SocialMediaIcons.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="flex items-center justify-center gap-2">
-    <a
-      href="https://www.facebook.com/ADinQAB/?fref=ts"
-      target="_blank"
-      rel="noopener"
-    >
+    <a href="https://www.facebook.com/" target="_blank" rel="noopener">
       <Icon
         name="fa:facebook-square"
         class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
       />
     </a>
 
-    <a
-      href="https://www.linkedin.com/company/adinq-ab/?originalSubdomain=se"
-      target="_blank"
-      rel="noopener"
-    >
+    <a href="https://www.linkedin.com/" target="_blank" rel="noopener">
       <Icon
         name="fa:linkedin-square"
         class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
       />
     </a>
 
-    <a
-      href="https://www.youtube.com/channel/UCnc_HP_7V8zWUTZBpGRdp2w/videos"
-      target="_blank"
-      rel="noopener"
-    >
+    <a href="https://www.youtube.com/" target="_blank" rel="noopener">
       <Icon
         name="fa:youtube-square"
         class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
