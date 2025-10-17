7043b218b6b52fd85c35bb71e4f053259c66100d
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-10-17 16:40:49 +0200

    changed to nuxt 4 structure

--- app/router.options.ts
+++ app/router.options.ts
@@ -5,10 +5,21 @@ export default {
     }
 
     if (to.hash) {
+      setTimeout(() => {
+        window.history.replaceState(
+          history.state,
+          "",
+          window.location.pathname + window.location.search,
+        );
+      }, 800);
+
       return {
         el: to.hash,
+        top: 50,
         behavior: "smooth",
       };
     }
+
+    return { top: 0 };
   },
 };
