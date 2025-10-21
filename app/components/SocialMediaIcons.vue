4570486d55413781c1a0dd8c51084890d2eadc80
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2025-10-21 22:40:45 +0200

    removed youtube and updated fb and insta

--- app/components/SocialMediaIcons.vue
+++ app/components/SocialMediaIcons.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="flex items-center justify-center gap-2">
-    <a href="https://www.facebook.com/" target="_blank" rel="noopener">
+    <a
+      href="https://www.facebook.com/profile.php?id=61576869093029"
+      target="_blank"
+      rel="noopener"
+    >
       <IconFacebook
         class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
       />
     </a>
 
-    <a href="https://www.linkedin.com/" target="_blank" rel="noopener">
+    <a
+      href="https://www.instagram.com/7h_stallningab/"
+      target="_blank"
+      rel="noopener"
+    >
       <IconLinkedin
-        class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
-      />
-    </a>
-
-    <a href="https://www.youtube.com/" target="_blank" rel="noopener">
-      <IconYoutube
         class="h-6 min-h-6 w-6 min-w-6 opacity-50 transition-opacity duration-300 hover:opacity-100"
       />
     </a>
