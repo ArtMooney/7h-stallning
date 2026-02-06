4d5557585f5549525ba9bccfdf29cfeafd167106
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2026-02-06 18:00:35 +0100

    modified to fit with cms content

--- app/components/PointBlob.vue
+++ app/components/PointBlob.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="h-fit w-fit">
     <div class="gap-2 p-8">
-      <h4 v-if="title" :class="getPointWidth">{{ title }}</h4>
+      <h4 v-if="title">{{ title }}</h4>
 
-      <ul v-if="pointLines" class="flex flex-col space-y-4 leading-normal">
-        <li v-for="line in pointLines">
-          <span v-if="getPointSymbol" class="absolute" :class="getPointWidth">{{
-            getPointSymbol
-          }}</span>
-          <span v-html="line"></span>
-        </li>
-      </ul>
+      <div v-html="splitText(text)" class="[&_p]:mb-2"></div>
     </div>
   </div>
 </template>
@@ -24,33 +17,20 @@ export default {
       type: String,
       required: false,
     },
-    pointLines: {
-      type: Array,
-      required: false,
-    },
-    pointType: {
+    text: {
       type: String,
-      required: false,
-    },
-    pointWidth: {
-      type: String, // small, medium, large
       required: false,
     },
   },
 
-  computed: {
-    getPointSymbol() {
-      if (this.pointType === "dot") return "•";
-      if (this.pointType === "checkbox") return "✅";
-      if (this.pointType === "arrow") return "→";
-      return "";
-    },
+  methods: {
+    splitText(text) {
+      const lines = formatText(text).split("<br>");
+      const elements = lines.map(
+        (line) => `<p >${line.replace(/\*/g, "•")}</p>`,
+      );
 
-    getPointWidth() {
-      if (this.pointWidth === "small") return "-ml-3";
-      if (this.pointWidth === "medium") return "-ml-6";
-      if (this.pointWidth === "large") return "-ml-9";
-      return "";
+      return elements.join("");
     },
   },
 };
